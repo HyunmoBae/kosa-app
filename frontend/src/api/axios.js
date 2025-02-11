858b60535820c415932b8d5ccb4fc87945585022
root <root@storage.example.com>
2025-02-11 18:26:42 +0900

    UPDATE frontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Axios 인스턴스 생성
 const api = axios.create({
-  baseURL: "http://api-gateway:3000/api",
+  baseURL: process.env.REACT_APP_API_URL,
   timeout: 30000,
   headers: {
     'Content-Type': 'application/json',
